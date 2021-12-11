8634436eb449de0c92387b04c7803cad81d1e627
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2021-12-11 13:00:41 +0100

    fix: remove unused import

--- src/de/durchfasten/listeners/JoinListener.java
+++ src/de/durchfasten/listeners/JoinListener.java
@@ -6,8 +6,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import com.earth2me.essentials.api.UserDoesNotExistException;
-
 import de.durchfasten.utils.Manager;
 
 public class JoinListener implements Listener{
